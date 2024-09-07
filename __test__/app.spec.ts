3e79450c98810b4e88ff0dd0e86bc403932e9028
vanzuni <vanessa.zuniga@editrade.cl>
2024-09-07 10:59:10 -0400

    fix test

--- __test__/app.spec.ts
+++ __test__/app.spec.ts
@@ -21,6 +21,7 @@ describe("Test Suite App", () => {
         expect(1 + 1).toBe(2);
     });
 
+ 
     test("test de endpoint /", async () => {
         return await request(app)
             .get("/")
@@ -29,5 +30,65 @@ describe("Test Suite App", () => {
             .then((response) => {
                 expect(response.text).toBe(`Hola, esta api fue configurada por el usuario ${configuration.username}`);
             })
+    });
+    
+
+    test("test de endpoint /palindromo/:frase", async () => {
+        const frase = 'radar';  
+        return await request(app)
+            .get(`/palindromo/${frase}`)
+            .expect("Content-Type", /text/)
+            .expect(200)
+            .then((response) => {
+                expect(response.text).toBe(`Hola, La frase ingresada es palindromo`);
+            });
+    });
+
+
+    test("test de endpoint /palindromo/:frase", async () => {
+        const frase = 'hola';  
+        return await request(app)
+            .get(`/palindromo/${frase}`)
+            .expect("Content-Type", /text/)
+            .expect(200)
+            .then((response) => {
+                expect(response.text).toBe(`Hola, La frase ingresada no es palindromo`);
+            });
+    });
+
+    test("test de endpoint /primo/:numero", async () => {
+        const numeroPrimo = 7;        
+        return await request(app)
+            .get(`/primo/${numeroPrimo}`)
+            .expect("Content-Type", /text/)
+            .expect(200)
+            .then((response) => {
+                expect(response.text).toBe(`Hola, el numero ingresado es un numero primo`);
+            });        
+         
+    });
+
+    test("test de endpoint /primo/:numero con un número no primo", async () => {
+        const numeroNoPrimo = 4; 
+        
+        return await request(app)
+            .get(`/primo/${numeroNoPrimo}`)
+            .expect("Content-Type", /text/)
+            .expect(200)
+            .then((response) => {
+                expect(response.text).toBe(`Hola, el numero ingresado no es un numero primo`);
+            });
+    });
+
+    test("test de endpoint /primo/:numero con un número no primo", async () => {
+        const numeroNoPrimo = 1;  
+        
+        return await request(app)
+            .get(`/primo/${numeroNoPrimo}`)
+            .expect("Content-Type", /text/)
+            .expect(200)
+            .then((response) => {
+                expect(response.text).toBe(`Hola, el numero ingresado no es un numero primo`);
+            });
     });
 });
